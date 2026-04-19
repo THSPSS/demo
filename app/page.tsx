4146946bab6ebb580796a77046a8c1618348fa22
THSPSS <sunyeongbang98@gmail.com>
2026-04-19 22:52:06 +0900

    update tosspayments pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
 "use client";
-export default function Home() {
-  return (<button onClick={() => console.log("결제하기 클릭")}>결제하기</button>);
+
+import { useRouter } from "next/navigation";
+
+export default function HomePage() {
+  const router = useRouter();
+  return (<>
+    <p>Product Name : living water</p>
+    <p>Price : 0 won</p>
+    <p>Description : Giving Eterneal life and everyone who drinks it never thirst again</p>
+    <button onClick={() => router.push("/payments")}>새로운 페이지로 이동</button></>);
+
 }
---
+++ app/payments/page.tsx
@@ -0,0 +1,5 @@
+import { WidgetCheckoutPage } from "./widget-checkout";
+
+export default function PaymentsPage() {
+    return <><h1>paymenet page</h1><WidgetCheckoutPage /></>;
+}
