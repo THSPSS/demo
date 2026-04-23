2dad9bb1904726ea491e3403a113f2538e3d7257
THSPSS <sunyeongbang98@gmail.com>
2026-04-23 22:33:15 +0900

    update app main page

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,6 @@ export default function HomePage() {
     <p>Product Name : living water</p>
     <p>Price : 0 won</p>
     <p>Description : Giving Eterneal life and everyone who drinks it never thirst again</p>
+    <p>Description : Giving Eterneal life and everyone who drinks it never thirst again</p>
     <button onClick={() => router.push("/payments")}>새로운 페이지로 이동</button></>);
-
 }
